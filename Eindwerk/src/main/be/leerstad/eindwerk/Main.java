27b64192d207654e4e3dd24c7671ab80f2b5b6e3
Jonathandlm <jonathandlm@hotmail.com>
2017-03-19 08:46:09 +0100

    main view

--- Eindwerk/src/main/be/leerstad/eindwerk/Main.java
+++ Eindwerk/src/main/be/leerstad/eindwerk/Main.java
@@ -4,14 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/Main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
         primaryStage.setTitle("Hello World");
+        primaryStage.getIcons().add(new Image("/logo.png"));
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
--- Eindwerk/src/main/be/leerstad/eindwerk/view/MainController.java
+++ Eindwerk/src/main/be/leerstad/eindwerk/view/MainController.java
@@ -1,24 +1,48 @@
 package be.leerstad.eindwerk.view;
 
-import javafx.event.ActionEvent;
+import javafx.application.Platform;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import org.controlsfx.control.StatusBar;
+
+import java.io.File;
 
 public class MainController {
 
-    public TextField voornaam;
-    public TextField achternaam;
-    public Label resultaat;
+    public TextField firstName;
+    public TextField name;
+    public Label result;
+    public StatusBar statusBar;
 
-    public void handleButtonAction(ActionEvent event) {
-        resultaat.setText(nummerUitNaam(voornaam.getText(),achternaam.getText()));
+    public void handleButtonAction() {
+        result.setText(createUsername(firstName.getText(), name.getText()));
     }
 
-    private String nummerUitNaam (String voornaam, String achternaam) {
+    private String createUsername(String firstName, String name) {
         StringBuilder result = new StringBuilder();
-        if (voornaam.length() >= 1) result.append(voornaam.charAt(0));
-        if (achternaam.length() >= 7) result.append(achternaam.substring(0,7));
-        else if (achternaam.length() >= 1) result.append(achternaam);
+        if (firstName.length() >= 1) result.append(firstName.charAt(0));
+        if (name.length() >= 7) result.append(name.substring(0,7));
+        else if (name.length() >= 1) result.append(name);
         return result.toString();
+    }
+
+    public void openFile() {
+        FileChooser chooser = new FileChooser();
+        File file = chooser.showOpenDialog(result.getScene().getWindow());
+        if (file != null) {
+            result.setText(file.getName());
+            statusBar.setText("File successfully opened!");
+        }
+    }
+
+    public void exitProgram() {
+        Platform.exit();
+    }
+
+    public void generateVisitsReport() {
+    }
+
+    public void generateSessionsReport() {
     }
 }
